4098ae221f2557e3f419ea327cf7eb8f3f5f9351
Jake Murray <j1993murray@hotmail.com>
2024-11-13 11:58:01 +0000

    Update: fxmanifest
    - Reorder manifest
    - Add version number

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
-description 'VibrantRP Fleeca Heist'
+lua54 'yes'
+
+name 'vib_fleecaheist'
+author 'Vibrant Resources'
+version '1.0.2'
+repository 'https://github.com/VibrantResources/vib_fleecaheist'
+description 'A small heist simulkator targeting Fleeca banks with varying hack requirements, armed guard respoonses and randomised rewards'
 
 client_scripts {
 	'client/*.lua',
@@ -15,5 +21,3 @@ shared_scripts {
     '@ox_lib/init.lua',
 	'config.lua',
 }
-
-lua54 'yes'
