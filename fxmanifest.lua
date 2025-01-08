ba8bc5c38d46824c2fb8736f2a1723cef9e5f8f7
Jake Murray <j1993murray@hotmail.com>
2025-01-08 22:36:09 +0000

    Fix: Typo in README

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,9 @@ lua54 'yes'
 
 name 'vib_fleecaheist'
 author 'Vibrant Resources'
-version '1.0.2'
+version '1.1.0'
 repository 'https://github.com/VibrantResources/vib_fleecaheist'
-description 'A small heist simulator targeting Fleeca banks with varying hack requirements, armed guard respoonses and randomised rewards'
+description 'A small heist simulator targeting Fleeca banks with varying hack requirements and randomised rewards'
 
 client_scripts {
 	'client/*.lua',
