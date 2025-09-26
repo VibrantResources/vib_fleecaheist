52a302269dc2d030f957d8abefae7daeca64c7e5
Vibrant <127861623+VibrantRP@users.noreply.github.com>
2025-09-26 18:26:22 +0100

    Update: Compress hack list (Another one)

--- client/big_hack_list.lua
+++ client/big_hack_list.lua
@@ -5,35 +5,35 @@ local bl_ui = exports.bl_ui
 -- Details for the minigames can be found in BL's documentation, with a link in our README
 
 RegisterNetEvent('banks:client:AttemptToUploadSoftwareToDevice', function(data)
-    local player = cache.ped
-    local success = nil
+    local hacks = {
+        circle_progress = { fn = bl_ui.CircleProgress, args = {2, 2} },
+        normal_progress = { fn = bl_ui.Progress,      args = {"rand1", "rand2"} },
+        key_spam        = { fn = bl_ui.KeySpam,       args = {"rand1", "rand2"} },
+        key_circle      = { fn = bl_ui.KeyCircle,     args = {"rand1", "rand2", 3} },
+        number_slide    = { fn = bl_ui.NumberSlide,   args = {"rand1", "rand2", 3} },
+        rapid_lines     = { fn = bl_ui.RapidLines,    args = {"rand1", "rand2", 3} },
+        circle_shake    = { fn = bl_ui.CircleShake,   args = {"rand1", "rand2", 3} },
+    }
 
-    if data.hackToInstall == "circle_progress" then
-        success = bl_ui:CircleProgress(2,2)
-    end
+    local chosenHack = hacks[data.hackToInstall]
+    local success = false
 
-    if data.hackToInstall == "normal_progress" then
-        success = bl_ui:Progress(math.random(3, 5), math.random(50, 100))
-    end
+    if chosenHack then
+        local option1 = math.random(3, 5)
+        local option2 = math.random(50, 100)
 
-    if data.hackToInstall == "key_spam" then
-        success = bl_ui:KeySpam(math.random(3, 5), math.random(50, 100))
-    end
-    
-    if data.hackToInstall == "key_circle" then
-        success = bl_ui:KeyCircle(math.random(3, 5), math.random(50, 100), 3)
-    end
+        local args = {}
+        for i, v in ipairs(chosenHack.args) do
+            if v == "rand1" then
+                args[i] = option1
+            elseif v == "rand2" then
+                args[i] = option2
+            else
+                args[i] = v
+            end
+        end
 
-    if data.hackToInstall == "number_slide" then
-        success = bl_ui:NumberSlide(math.random(3, 5), math.random(50, 100), 3)
-    end
-
-    if data.hackToInstall == "rapid_lines" then
-        success = bl_ui:RapidLines(math.random(3, 5), math.random(50, 100), 3)
-    end
-
-    if data.hackToInstall == "circle_shake" then
-        success = bl_ui:CircleShake(math.random(3, 5), math.random(50, 100), 3)
+        success = chosenHack.fn(bl_ui, table.unpack(args))
     end
 
     if success then
